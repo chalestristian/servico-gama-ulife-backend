02aa4a792bcc461d4b5695cfb7a9bf381ab90457
unknown <luquinha207@hotmail.com>
2022-03-27 00:47:43 -0300

    Configurando connection string banco de dados

--- servico-gama-ulife/Repository/ApiTesteRepository.cs
+++ servico-gama-ulife/Repository/ApiTesteRepository.cs
@@ -1,12 +1,57 @@
 ﻿using servico_gama_ulife.Repository.Interface;
+using Microsoft.Extensions.Configuration;
+using servico_gama_ulife.Repository.Configuration;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace servico_gama_ulife.Repository
 {
-    public class ApiTesteRepository : IApiTesteRepository
+    public class ApiTesteRepository : PostgreSqlBase, IApiTesteRepository
     {
+        public ApiTesteRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
         public string ApiTesteBanco()
         {
             return "Bem vindo ao teste!";
+        }
+
+        public void ExecutarUpdateOuInsertNoBanco()
+        {
+            string sql = $@"Sua query aqui !!!";
+
+            using (var connection = GetConnection() as NpgsqlConnection)
+            {
+                Execute(sql, connection);
+            }
+        }
+
+        public IEnumerable<object> BuscarDadosNoBancoAtravesDeUmObjetoEspecifico(int codigo)
+        {
+            IEnumerable<object> teste = null;
+            string sql = $@"Sua query aqui !! {codigo}";
+
+            using (var connection = GetConnection() as NpgsqlConnection)
+            {
+                teste = Query<object>(sql, connection);
+
+            }
+            return teste;
+        }
+
+        public object BuscarUmUnicoDadoNoBanco(int codigo)
+        {
+            object teste = null;
+            string sql = $@"Sua query aqui{codigo}";
+
+            using (var connection = GetConnection() as NpgsqlConnection)
+            {
+                teste = Query<object>(sql, connection).FirstOrDefault();
+
+            }
+            return teste;
         }
     }
 }
---
+++ servico-gama-ulife/Repository/Configuration/PostgreSqlBase.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Collections.Generic;
+using System.Data;
+
+namespace servico_gama_ulife.Repository.Configuration
+{
+    public class PostgreSqlBase
+    {
+        private readonly IConfiguration _configuration;
+
+        protected PostgreSqlBase(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            string connectionString = _configuration.GetConnectionString("ServicoGamaUlife");
+            IDbConnection dbConnection = new NpgsqlConnection(connectionString);
+            dbConnection.Open();
+
+            return dbConnection;
+        }
+        protected virtual int Execute(string sql, IDbConnection connection, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            var result = connection.Execute(sql, param, transaction, commandTimeout, commandType: commandType);
+            return result;
+        }
+
+        protected virtual IEnumerable<T> Query<T>(string sql, IDbConnection connection, object param = null, CommandType? commandType = null) where T : new()
+        {
+            IEnumerable<T> result = connection.Query<T>(sql, param, commandType: commandType);
+            return result;
+        }
+    }
+}
