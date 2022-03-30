345110f2d7d92e9d807aac2b12f5b578110a32b9
Milena Ferraz <milena.ferraz@animaeducacao.com.br>
2022-03-30 09:17:56 -0300

    Validação UpdateUser

--- servico-gama-ulife/Service/UserService.cs
+++ servico-gama-ulife/Service/UserService.cs
@@ -1,7 +1,6 @@
 ﻿using servico_gama_ulife.Model;
 using servico_gama_ulife.Repository.Interface;
 using servico_gama_ulife.Service.Request;
-using System;
 using System.Collections.Generic;
 
 namespace servico_gama_ulife.Service
@@ -37,9 +36,7 @@ namespace servico_gama_ulife.Service
 
         public IList<UserModel> GetAllByType(int nr_type)
         {
-            return (nr_type == (int)Enum.TypeUser.Estudante || nr_type == (int)Enum.TypeUser.Professor)
-                      ? _userRepository.GetAllByType(nr_type)
-                      : throw new Exception("Tipo de usuário não encontrado!");
+            return _userRepository.GetAllByType(nr_type);
         }
     }
 }
---
+++ servico-gama-ulife/Validators/UpdateUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using servico_gama_ulife.Service.Request;
+
+namespace servico_gama_ulife.Validators
+{
+    public class UpdateUserValidator : AbstractValidator<EditUser>
+    {
+        public UpdateUserValidator() 
+        {
+            RuleFor(p => p.Ds_email)
+               .NotNull().WithMessage("E-mail obrigatório!")
+               .EmailAddress().WithMessage("Este e-mail não é válido!");
+
+            RuleFor(p => p.Nm_user)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(5).WithMessage("Deve ter no mínimo de 5 caracteres!")
+                .WithMessage("Nome é obrigatório!");
+        }
+    }
+}
